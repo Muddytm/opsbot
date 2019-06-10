0af6b7cdf87cfcde2767767a968b68dd6035b261
calebh93 <hawkins.caleb93@gmail.com>
2019-06-10 14:58:57 -0700

    Trying this method out

--- opsbot/commands.py
+++ opsbot/commands.py
@@ -166,24 +166,27 @@ def notify(message):
                             for db, server in zip(dbs, servers):
                                 logging.info("{} reason=[NOTIFIED OF DATABASE ACCESS EXPIRING]\n".format(user["name"]), server, db, "notifyexpire")
 
-                # Set up reminder to log out of SQL.
-                userdata = None
-                for filename in os.listdir("userdata/"):
-                    if person.replace(".", "_") in filename:
-                        with open("userdata/{}".format(filename)) as data_file:
-                            userdata = json.load(data_file)
-
-                        # Send "log out of SQL server" message
-                        if "expired" in userdata and "new" in userdata["expired"]:
-                            message._client.send_message(chan,
-                                                         Strings["REMOVE_LOGIN"])
-                            userdata["expired"] = "old"
-
-                            with open("userdata/{}".format(filename), 'w') as outfile:
-                                json.dump(userdata, outfile)
-
             except Exception as e:
                 message._client.send_message(errors_channel, "```{}```".format(e))
+
+        # Set up reminder to log out of SQL.
+        users = hf.get_users()
+        for filename in os.listdir("userdata/"):
+            with open("userdata/{}".format(filename)) as data_file:
+                userdata = json.load(data_file)
+
+            # Send "log out of SQL server" message
+            if "expired" in userdata and "new" in userdata["expired"]:
+                for user in users:
+                    if user["name"].replace("_", ".") == filename.replace("_active.json", ""):
+                        chan = hf.find_channel(message._client.channels, user["id"])
+
+                        message._client.send_message(chan,
+                                                     Strings["REMOVE_LOGIN"])
+                        userdata["expired"] = "old"
+
+                        with open("userdata/{}".format(filename), 'w') as outfile:
+                            json.dump(userdata, outfile)
 
         # For use with Datadog
         with open("/opt/opsbot35/data/status.txt", "w") as f:
