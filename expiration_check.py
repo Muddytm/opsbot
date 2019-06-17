42d43ff4719ddcf86acc7a5eba90a26957bb146b
calebh93 <hawkins.caleb93@gmail.com>
2019-06-17 15:02:51 -0700

    Update expiration_check.py

--- expiration_check.py
+++ expiration_check.py
@@ -74,11 +74,14 @@ def execute_sql(sql, server, database=None, get_rows=False, userdata=None):
                 with open("data/jobs.json") as f:
                     jobs = json.load(f)
 
-                if "{}:{}".format(userdata["name"], server) not in jobs and "{}:{}:DONE".format(userdata["name"], server) not in jobs:
-                    jobs.append("{}:{}".format(userdata["name"], server))
+                for job in jobs:
+                    if job.startswith("{}:{}".format(userdata["name"], server)):
+                        continue
+                    else:
+                        jobs.append("{}:{}".format(userdata["name"], server))
 
-                    with open("data/jobs.json", "w") as f:
-                        json.dump(jobs, f)
+                with open("data/jobs.json", "w") as f:
+                    json.dump(jobs, f)
 
                 return None, userdata
             elif "it does not exist" in e.args[1]:
